f21e4b79abfd5c394d8ea0181b2dcd808d050656
Matt Fellows <matt.fellows@onegeek.com.au>
2015-02-12 13:19:14 +1100

    Short-circuit logic applied in the leave_domain action

--- lib/vagrant-windows-domain/action/leave_domain.rb
+++ lib/vagrant-windows-domain/action/leave_domain.rb
@@ -24,33 +24,32 @@ module VagrantPlugins
 
       def call(env)
 
-        if [:not_created].include? @machine.state.id
-          @logger.debug("Machine not created, nothing to do")
-        elsif [:running].include? @machine.state.id
-          answer = @machine.env.ui.ask("Are you sure you want to destroy this machine and disconnect from #{@config.domain}? (y/n)")
-          if answer.downcase == 'y'
-            env[:force_confirm_destroy] = true # Prevent the popup dialog again
+        if @config and @config.include? "domain"
 
-            # OK, you want to kill. But do we have a valid config?
-            if @config
-              @logger.debug("Configuration detected, triggering leave domain action")
-              @provisioner.destroy
-            else
-              @logger.debug("No configuration detected, not leaving any domain")
-            end
+       	  if [:not_created].include? @machine.state.id
+       	    @logger.debug("Machine not created, nothing to do")
+       	  elsif [:running].include? @machine.state.id
+       	    answer = @machine.env.ui.ask("Are you sure you want to destroy this machine and disconnect from #{@config.domain}? (y/n)")
+       	    if answer.downcase == 'y'
+       	    	env[:force_confirm_destroy] = true # Prevent the popup dialog again
+       	    	@logger.debug("Valid configuration detected, triggering leave domain action")
+       	    	@provisioner.destroy
+       	    end
+       	  # elsif [:saved, :paused, :poweroff].include? @machine.state.id
+       	  else
+       	    @machine.env.ui.say(:warn, "Machine is currently not running. To properly leave the #{@config.domain} network the machine needs to be running and connected to the network in which it was provisioned. Please run `vagrant up` and then `vagrant destroy`.\n")
+       	    answer = @machine.env.ui.ask("Would you like to continue destroying this machine, leaving this machine orphaned in the '#{@config.domain}' network? (y/n)")
+       	    return unless answer.downcase == 'y' # Bail out of destroy and prevent middleware from continuing on
 
-          end
-        # elsif [:saved, :paused, :poweroff].include? @machine.state.id
-        else
-          @machine.env.ui.say(:warn, "Machine is currently not running. To properly leave the #{@config.domain} network the machine needs to be running and connected to the network in which it was provisioned. Please run `vagrant up` and then `vagrant destroy`.\n")
-          answer = @machine.env.ui.ask("Would you like to continue destroying this machine, leaving this machine orphaned in the '#{@config.domain}' network? (y/n)")
-          return unless answer.downcase == 'y' # Bail out of destroy and prevent middleware from continuing on
+       	    # OK, we're being naughty and letting the rest of the middleware do their things (i.e. destroy the machine, and such)
 
-          env[:force_confirm_destroy] = true # Prevent the popup dialog again
-
-          @logger.debug("Force destroying this machine and not leaving the domain #{@config.domain}")
-          @machine.env.ui.say(:warn, "Force destroying this machine and not leaving the domain #{@config.domain}. May FSM have mercy on your soul.")
-        end
+       	    env[:force_confirm_destroy] = true # Prevent the popup dialog again
+       	    @logger.debug("Force destroying this machine and not leaving the domain #{@config.domain}")
+       	    @machine.env.ui.say(:warn, "Force destroying this machine and not leaving the domain #{@config.domain}. May FSM have mercy on your soul.")
+       	  end
+       	else
+       	  @logger.debug("No configuration detected, not leaving any domain")
+       	end
 
         # Continue the rest of the middleware actions
         @app.call(env)
